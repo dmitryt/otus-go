3d3bd1479b8a9dee47f64487751491e0638b7543
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2019-09-10 22:19:42 +0200

    hw1

---
+++ hw-1/go.mod
@@ -0,0 +1,9 @@
+module github.com/dmitryt/otus-go/hw-1
+
+go 1.13
+
+require (
+	github.com/beevik/ntp v0.2.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20190909003024-a7b16738d86b // indirect
+)
---
+++ hw-1/time.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error occured: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Hello, now is %s", time);
+}
